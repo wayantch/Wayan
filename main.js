7847da63451b66d73004896d5c34870c91b29b4d
Wayan Deva <129466090+wayantch@users.noreply.github.com>
2023-11-29 18:51:56 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+const navbarNav = document.querySelector
+('.navbar');
+
+
+
+
+
+
+\
